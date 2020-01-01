cd6bff2e701642347c8ff8553550541edaea0b94
chernig <cherno_9@mail.ru>
2020-01-01 12:27:29 +1100

    Updates

---
+++ precache-manifest.305f209d769d83bd7eba87b4403caab0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896dbe3671567f39d25956af3fc0282f",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "4fa15995fcc991bd565f",
+    "url": "/web/static/css/2.b80cc862.chunk.css"
+  },
+  {
+    "revision": "8a2bd5fd40547d6ca29d",
+    "url": "/web/static/css/main.0d890ccb.chunk.css"
+  },
+  {
+    "revision": "4fa15995fcc991bd565f",
+    "url": "/web/static/js/2.96a2556d.chunk.js"
+  },
+  {
+    "revision": "a73788eb2ed0299b5b377f1e5de672b9",
+    "url": "/web/static/js/2.96a2556d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a2bd5fd40547d6ca29d",
+    "url": "/web/static/js/main.480663c9.chunk.js"
+  },
+  {
+    "revision": "70325b0dce548edce101",
+    "url": "/web/static/js/runtime-main.69fc4686.js"
+  },
+  {
+    "revision": "f27fd74a7df98167d09393c5c2da4e2a",
+    "url": "/web/static/media/back_3_fin.f27fd74a.png"
+  },
+  {
+    "revision": "e94eadf110f6e003b5afdcb0561d6034",
+    "url": "/web/static/media/back_4.e94eadf1.jpg"
+  },
+  {
+    "revision": "4e9a36a2c68bcfbe6ee3ffee2ef8027e",
+    "url": "/web/static/media/copperplate-gothic-light.4e9a36a2.ttf"
+  },
+  {
+    "revision": "d0b86ee766794302fc780283033bb4bc",
+    "url": "/web/static/media/deep.d0b86ee7.jpg"
+  },
+  {
+    "revision": "2e000123f046247f8d4c13cebb04cbe8",
+    "url": "/web/static/media/deep_1.2e000123.png"
+  },
+  {
+    "revision": "1efbff3e0850c046f03893defd4bde23",
+    "url": "/web/static/media/deep_2.1efbff3e.png"
+  },
+  {
+    "revision": "d6687210a3bcdf72e37ecf1d0452c3a5",
+    "url": "/web/static/media/deep_3.d6687210.png"
+  },
+  {
+    "revision": "d3cc84bbe0dc8fe63a9270e28c18841d",
+    "url": "/web/static/media/deep_4.d3cc84bb.png"
+  },
+  {
+    "revision": "d433f55099afb266d500edac12e74cc7",
+    "url": "/web/static/media/deep_save.d433f550.jpg"
+  },
+  {
+    "revision": "6108b89a67634c363d63480e8bb9ce51",
+    "url": "/web/static/media/finance.6108b89a.jpg"
+  },
+  {
+    "revision": "b4fec40d0a449f7fd637877882c994e1",
+    "url": "/web/static/media/finance_1.b4fec40d.png"
+  },
+  {
+    "revision": "ace9cc8a39c799ae082a2b4c6f67eec8",
+    "url": "/web/static/media/finance_2.ace9cc8a.png"
+  },
+  {
+    "revision": "1004f2950de0217d56a9008f6ce2c1be",
+    "url": "/web/static/media/finance_3.1004f295.png"
+  },
+  {
+    "revision": "f30da107ee5be1a1698ba47ea57f6e0e",
+    "url": "/web/static/media/logo1.f30da107.png"
+  },
+  {
+    "revision": "77ebd0063ee3f35ff1e3d3b172540eda",
+    "url": "/web/static/media/snake.77ebd006.jpg"
+  },
+  {
+    "revision": "8c8480ba8c1014ad5f74ac3da7624310",
+    "url": "/web/static/media/snake_1.8c8480ba.png"
+  },
+  {
+    "revision": "5c80ab61c780090fa6078b6dcc7baab6",
+    "url": "/web/static/media/snake_2.5c80ab61.png"
+  },
+  {
+    "revision": "6ac763bb4370b9cc639add436cd2a0e4",
+    "url": "/web/static/media/website.6ac763bb.jpg"
+  }
+]);
